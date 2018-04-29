4424546fcf7cf9c64c4c858ba050a0fb090f11b2
Jon Borgonia <jon@gomagames.com>
2018-04-29 11:21:57 -0700

    add Immutable js and reducers

--- src/app.tsx
+++ src/app.tsx
@@ -1,121 +1,114 @@
 import xs from 'xstream'
+import { Map } from 'immutable'
 import {Sources, Sinks} from './interfaces'
-
-const initialState = {
-  step : 0,
-  desired : {
-    people : false,
-    product : false,
-    tech : false,
-    learn : false,
-    pay : false,
-    freedom : false,
-    proficiency : false
-  },
-  current : {
-    people : false,
-    product : false,
-    tech : false,
-    learn : false,
-    pay : false,
-    freedom : false,
-    proficiency : false
-  }
-}
 
 export function App(sources : Sources) : Sinks {
 
-  const toggle = ( select:any, label:string ) => select.events('click')
-    .map(( ev:any ) => ({ 
-      label, 
-      val : ev.target.dataset.selected === "false" ? true : false 
-    }))
-    .startWith({ label, val : false });
-
-  const [
-    lovePeople$,
-    loveProduct$,
-    loveTech$,
-    learn$,
-    rewardPay$,
-    rewardFreedom$,
-    proficiency$
-  ] = [
-    ['love-people', 'People'],
-    ['love-product', 'Product'],
-    ['love-tech', 'Tech'],
-    ['learn', 'Learn'],
-    ['reward-pay', 'Pay'],
-    ['reward-freedom', 'Freedom'],
-    ['proficiency', 'Proficiency']
-  ].map(([selector, label]) => toggle(sources.DOM.select('.'+selector), label).map(({ val, label }) =>
-    <button className={`button ${val ? '' : 'button-outline'} ${selector} selected-${val}`} data-selected={val}>{ label }</button>
-  ));
-
-  const current$ = xs.combine(lovePeople$, loveProduct$, loveTech$, learn$, rewardPay$, rewardFreedom$, proficiency$)
-    .map(([ lovePeople, loveProduct, loveTech, learn, rewardPay, rewardFreedom, proficiency ]) => <ul>
-        <li>
-          <h2>Love</h2>
-          { lovePeople }
-          { loveProduct }
-          { loveTech }
-        </li>
-        <li>
-          <h2>Learn</h2>
-          { learn }
-        </li>
-        <li>
-          <h2>Reward</h2>
-          { rewardPay }
-          { rewardFreedom }
-        </li>
-        <li>
-          <h2>Proficiency</h2>
-          { proficiency }
-        </li>
-      </ul>
-    )
-
-  const prospective$ = xs.combine(lovePeople$, loveProduct$, loveTech$, learn$, rewardPay$, rewardFreedom$, proficiency$)
-    .map(([ lovePeople, loveProduct, loveTech, learn, rewardPay, rewardFreedom, proficiency ]) => <ul>
-        <li>
-          <h2>Love</h2>
-          { lovePeople }
-          { loveProduct }
-          { loveTech }
-        </li>
-        <li>
-          <h2>Learn</h2>
-          { learn }
-        </li>
-        <li>
-          <h2>Reward</h2>
-          { rewardPay }
-          { rewardFreedom }
-        </li>
-        <li>
-          <h2>Proficiency</h2>
-          { proficiency }
-        </li>
-      </ul>
-    )
-
+  const currentSituationStep = 1;
+  const desiredSituationStep = 5;
   const steps = [
-    <h2>Step 0</h2>,
-    <h2>Step 1</h2>,
-    <h2>Step 2</h2>
+    {
+      label : <div>
+        <h2>If you are currently working, <a href="#1" className="workingStart">start here</a>.</h2>
+        <h2>Otherwise, <a href="#2" className="unemployedStart">start here</a>.</h2>
+      </div>,
+      input : null
+    },
+    {
+      label : <div>
+        <p>
+          Where are you now?<br />
+          Think about your current job— who you work with, what you are working on, your professional environment, and your lifestyle.
+        </p>
+        <h2>What about your current position do you really like?</h2>
+        <p>(Select all that are true.)</p>
+      </div>,
+      input : <div>
+        <ul className="inputList">
+          <li>
+            <input type="checkbox" id="currentLikePeople" />
+            <label for="currentLikePeople" className="label-inline">
+              I really like the people I work with.
+            </label>
+          </li>
+          <li>
+            <input type="checkbox" id="currentLikeProduct" />
+            <label for="currentLikeProduct" className="label-inline">
+              I like the project/product I am working on, or I like purpose that the project/product I am working on serves.
+            </label>
+          </li>
+          <li>
+            <input type="checkbox" id="currentLikeTech" />
+            <label for="currentLikeTech" className="label-inline">
+              I like the tech stack I am using.
+            </label>
+          </li>
+        </ul>
+        <button className="currentLikeNext">Next</button>
+      </div>
+    }
   ];
 
-  const model = ({ dec$, dec2$ }) => {
-    return xs.merge(
-      dec$.mapTo({ step : 1 }).startWith({ step : 0 }),
-      dec2$.mapTo({ step : 2 }).startWith({ step : 0 })
-    );
+  const reducers = ({
+    workingStart$,
+    unemployedStart$,
+    currentLikePeople$,
+    currentLikeProduct$,
+    currentLikeTech$
+  }) => xs.merge(
+    workingStart$.mapTo(state => 
+      state.set('step', currentSituationStep)
+    ),
+    unemployedStart$.mapTo(state =>
+      state.set('step', desiredSituationStep )
+    ),
+    currentLikePeople$.map(e => state => 
+      state.setIn(['current','people'], e.target.checked)
+    ),
+    currentLikeProduct$.map(e => state => 
+      state.setIn(['current','product'], e.target.checked)
+    ),
+    currentLikeTech$.map(e => state => 
+      state.setIn(['current','tech'], e.target.checked)
+    )
+  );
+
+  const model = actions => {
+    const reducer$ = reducers(actions);
+
+    const initialState = Map({
+      step : 0,
+      currentlyWorking : false,
+      current : Map({
+        people : false,
+        product : false,
+        tech : false,
+        learn : false,
+        pay : false,
+        freedom : false,
+        proficiency : false
+      }),
+      desired : Map({
+        people : false,
+        product : false,
+        tech : false,
+        learn : false,
+        pay : false,
+        freedom : false,
+        proficiency : false
+      })
+    });
+
+    const state$ = reducer$.fold((next, reducer) => reducer(next), initialState);
+    return state$;
   }
 
   const intent = DOM => ({
-    dec$ : sources.DOM.select('.dec').events('click'),
-    dec2$ : sources.DOM.select('.dec2').events('click')
+    workingStart$ : sources.DOM.select('.workingStart').events('click'),
+    unemployedStart$ : sources.DOM.select('.unemployedStart').events('click'),
+    currentLikePeople$ : sources.DOM.select('#currentLikePeople').events('change'),
+    currentLikeProduct$ : sources.DOM.select('#currentLikeProduct').events('change'),
+    currentLikeTech$ : sources.DOM.select('#currentLikeTech').events('change'),
   });
 
   const view = state$ => state$.map( state  => 
@@ -123,14 +116,13 @@ export function App(sources : Sources) : Sinks {
       <h1>Modus</h1>
 
       <div className="row">
-        { steps[state.step] }
+        { steps[state.get('step')].label }
       </div>
       <div className="row">
-        <button className="button dec">Set step 1</button>
-        <button className="button dec">Set A to 5</button>
-        <button className="button dec2">Set A to 10</button>
-        <button className="button inc">Set B to 5</button>
-        <button className="button inc2">Set B to 10</button>
+        { steps[state.get('step')].input }
+      </div>
+      <div>
+        { JSON.stringify(state) }
       </div>
     </div>
   )
